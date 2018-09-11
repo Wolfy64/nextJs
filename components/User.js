ae50afe6cfc1f30245ecf7bc16a783d70a3a8f80
David <ledavid64@gmail.com>
2018-09-10 22:11:50 -0700

    Styling our App in Next.js

--- components/User.js
+++ components/User.js
@@ -4,6 +4,16 @@ const user = props => (
   <div>
     <h1>{props.name}</h1>
     <p>Age: {props.age}</p>
+    <style jsx>{`
+      div {
+        background-color: papayawhip;
+        border: 1px solid palevioletred;
+        box-shadow: 0 2p 3px #ccc;
+        padding: 20px;
+        text-align: center;
+        color: palevioletred;
+      }
+    `}</style>
   </div>
 );
 
